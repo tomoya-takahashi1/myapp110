0825be0b00c0b0d9bca568a4e2cfcb4180aaa98b
Bts Scandal <btsscandal@BtsnoMacBook-Air.local>
2023-08-28 14:09:51 +0900

    README修正、インデント修正、バリデーション修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   validates :name, presence: true
-  validates :set, numericality: true
+ 
+  
+  validates :set, numericality: { greater_than_or_equal_to: 1 }
   belongs_to :user
   has_many :favorites, dependent: :destroy
 end
